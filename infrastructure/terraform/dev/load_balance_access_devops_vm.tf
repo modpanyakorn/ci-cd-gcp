b9491768ffbe4502f8a46ebfedc1866e7aefcb85
modpanyakorn <panyakorn.mod@gmail.com>
2025-08-26 04:23:14 +0700

    update: add gcp manage ssl cert (load balance need https)

--- infrastructure/terraform/dev/load_balance_access_devops_vm.tf
+++ infrastructure/terraform/dev/load_balance_access_devops_vm.tf
@@ -68,7 +68,7 @@ resource "google_compute_health_check" "sonarqube_health_check" {
   name = "sonarqube-lb-health-check"
   http_health_check {
     port         = 8003
-    request_path = "/api/system/health"
+    request_path = "/api/system/status"
   }
 }
 
@@ -77,7 +77,7 @@ resource "google_compute_health_check" "mongo_express_health_check" {
   name = "mongoexpress-lb-health-check"
   http_health_check {
     port         = 8004
-    request_path = "/health"
+    request_path = "/status"
   }
 }
 
@@ -173,68 +173,215 @@ resource "google_compute_url_map" "devops_url_map" {
   # Define Jenkins is main page
   default_service = google_compute_backend_service.devops_backend_iap["jenkins"].self_link
 
-  # reference pathmather for host_rules
+  # Main domain
   host_rule {
-    hosts        = ["*"]
-    path_matcher = "allpaths"
+    hosts        = var.domains
+    path_matcher = "main"
+  }
+  path_matcher {
+    name            = "main"
+    default_service = google_compute_backend_service.devops_backend_iap["jenkins"].self_link
   }
 
+  # Jenkins host rule
+  host_rule {
+    hosts        = var.jenkins_sub_domain
+    path_matcher = "jenkins-path-matcher"
+  }
+
+  # Path matcher for Jenkins
   path_matcher {
-    name            = "allpaths"
+    name            = "jenkins-path-matcher"
     default_service = google_compute_backend_service.devops_backend_iap["jenkins"].self_link
+  }
 
-    # Path Rule for Jenkins
-    path_rule {
-      paths   = ["/jenkins", "/jenkins/*"]
-      service = google_compute_backend_service.devops_backend_iap["jenkins"].self_link
-    }
+  # Grafana host rule
+  host_rule {
+    hosts        = var.grafana_sub_domain
+    path_matcher = "grafana-path-matcher"
+  }
 
-    # Path Rule for Grafana
-    path_rule {
-      paths   = ["/grafana", "/grafana/*"]
-      service = google_compute_backend_service.devops_backend_iap["grafana"].self_link
-    }
+  # Path Rule for Grafana
+  path_matcher {
+    name            = "grafana-path-matcher"
+    default_service = google_compute_backend_service.devops_backend_iap["grafana"].self_link
+  }
 
-    # Path Rule for Prometheus
-    path_rule {
-      paths   = ["/prometheus", "/prometheus/*"]
-      service = google_compute_backend_service.devops_backend_iap["prometheus"].self_link
-    }
+  # Prometheus host rule
+  host_rule {
+    hosts        = var.prometheus_sub_domain
+    path_matcher = "prometheus-path-matcher"
+  }
 
-    # Path Rule for Sonarqube
-    path_rule {
-      paths   = ["/sonarqube", "/sonarqube/*"]
-      service = google_compute_backend_service.devops_backend_iap["sonarqube"].self_link
-    }
+  # Path Rule for Prometheus
+  path_matcher {
+    name            = "prometheus-path-matcher"
+    default_service = google_compute_backend_service.devops_backend_iap["prometheus"].self_link
+  }
 
-    # Path Rule for Mongo Express
-    path_rule {
-      paths   = ["/mongoexpress", "/mongoexpress/*"]
-      service = google_compute_backend_service.devops_backend_iap["mongoexpress"].self_link
-    }
+  # Sonarqube host rule
+  host_rule {
+    hosts        = var.sonarqube_sub_domain
+    path_matcher = "sonarqube-path-matcher"
+  }
 
-    # Path Rule for cAdvisor
-    path_rule {
-      paths   = ["/cadvisor", "/cadvisor/*"]
-      service = google_compute_backend_service.devops_backend_iap["cadvisor"].self_link
-    }
+  # Path Rule for sonarqube
+  path_matcher {
+    name            = "sonarqube-path-matcher"
+    default_service = google_compute_backend_service.devops_backend_iap["sonarqube"].self_link
+  }
+
+  # Mongo Express host rule
+  host_rule {
+    hosts        = var.mongo_express_sub_domain
+    path_matcher = "mongo-express-path-matcher"
+  }
+
+  # Path Rule for Mongo Express
+  path_matcher {
+    name            = "mongo-express-path-matcher"
+    default_service = google_compute_backend_service.devops_backend_iap["mongoexpress"].self_link
+  }
+
+  # cAdvisor host rule
+  host_rule {
+    hosts        = var.cadvisor_sub_domain
+    path_matcher = "cadvisor-path-matcher"
+  }
+
+  # Path Rule for cAdvisor
+  path_matcher {
+    name            = "cadvisor-path-matcher"
+    default_service = google_compute_backend_service.devops_backend_iap["cadvisor"].self_link
   }
 }
 
 # HTTP proxy, receive traffic from forwarding_rule and sengind to backend service
+# non SSL
 resource "google_compute_target_http_proxy" "devops_http_proxy" {
   name    = "devops-lb-http-proxy"
   url_map = google_compute_url_map.devops_url_map.self_link # sending traffic to url mapping
 }
 
+# Managed SSL Certificate from GCP and allow domain
+# domain ssl
+resource "google_compute_managed_ssl_certificate" "default_service_ssl_cert" {
+  name = "default-service-ssl-cert"
+
+  managed {
+    domains = var.domains
+  }
+}
+
+resource "google_compute_managed_ssl_certificate" "devops_service_ssl_cert" {
+  for_each = {
+    # Combine all subdomains into one map for iteration
+    jenkins      = var.jenkins_sub_domain
+    grafana      = var.grafana_sub_domain
+    prometheus   = var.prometheus_sub_domain
+    sonarqube    = var.sonarqube_sub_domain
+    mongoexpress = var.mongo_express_sub_domain
+    cadvisor     = var.cadvisor_sub_domain
+  }
+  name = "${each.key}-ssl-cert"
+  managed {
+    domains = each.value
+  }
+}
+
+# Jenknins sub domain ssl
+# resource "google_compute_managed_ssl_certificate" "jenkins_ssl" {
+#   name = "jenkins-ssl-cert"
+
+#   managed {
+#     domains = var.jenkins_sub_domain
+#   }
+# }
+
+# # Grafana sub domain ssl
+# resource "google_compute_managed_ssl_certificate" "grafana_ssl" {
+#   name = "grafana-ssl-cert"
+
+#   managed {
+#     domains = var.grafana_sub_domain
+#   }
+# }
+
+# # Prometheus sub domain ssl
+# resource "google_compute_managed_ssl_certificate" "prometheus_ssl" {
+#   name = "prometheus-ssl-cert"
+
+#   managed {
+#     domains = var.prometheus_sub_domain
+#   }
+# }
+
+# # Sonarqube sub domain ssl
+# resource "google_compute_managed_ssl_certificate" "sonarqube_ssl" {
+#   name = "sonarqube-ssl-cert"
+
+#   managed {
+#     domains = var.sonarqube_sub_domain
+#   }
+# }
+
+# # Mongo Express sub domain ssl
+# resource "google_compute_managed_ssl_certificate" "mongo_express_ssl" {
+#   name = "mongo-express-ssl-cert"
+
+#   managed {
+#     domains = var.mongo_express_sub_domain
+#   }
+# }
+
+# # cAdvisor sub domain ssl
+# resource "google_compute_managed_ssl_certificate" "cadvisor_ssl" {
+#   name = "cadvisor-ssl-cert"
+#   managed {
+#     domains = var.cadvisor_sub_domain
+#   }
+# }
+
+# HTTPS proxy
+# SSL on
+resource "google_compute_target_https_proxy" "devops_https_proxy" {
+  name    = "devops-lb-https-proxy"
+  url_map = google_compute_url_map.devops_url_map.self_link
+
+  ssl_certificates = concat(
+    [google_compute_managed_ssl_certificate.default_service_ssl_cert.self_link],
+    [for cert in google_compute_managed_ssl_certificate.devops_service_ssl_cert : cert.self_link]
+  )
+
+  # ssl_certificates = [
+  #   google_compute_managed_ssl_certificate.default_service_ssl_cert.self_link,
+  # google_compute_managed_ssl_certificate.jenkins_ssl.id,
+  # google_compute_managed_ssl_certificate.grafana_ssl.id,
+  # google_compute_managed_ssl_certificate.prometheus_ssl.id,
+  # google_compute_managed_ssl_certificate.sonarqube_ssl.id,
+  # google_compute_managed_ssl_certificate.mongo_express_ssl.id,
+  # google_compute_managed_ssl_certificate.cadvisor_ssl.id
+  # ]
+}
+
 # Receive Traffic from Load Balancer (google_compute_global_address) on port80
 # Then sending traffic to http_proxy
+# Forwarding Rule HTTP (port 80) → http_proxy
 resource "google_compute_global_forwarding_rule" "devops_http_forwarding_rule" {
   name        = "devops-lb-http-forwarding-rule"
   ip_protocol = "TCP"
-  ip_address  = google_compute_global_address.devops_lb_static_ip.self_link
+  ip_address  = google_compute_global_address.devops_lb_static_ip.address
   target      = google_compute_target_http_proxy.devops_http_proxy.self_link
   port_range  = "80"
+}
+
+# Forwarding Rule HTTPS (port 443) → https_proxy
+resource "google_compute_global_forwarding_rule" "devops_https_forwarding_rule" {
+  name        = "devops-lb-https-forwarding-rule"
+  ip_protocol = "TCP"
+  ip_address  = google_compute_global_address.devops_lb_static_ip.address
+  target      = google_compute_target_https_proxy.devops_https_proxy.self_link
+  port_range  = "443"
 }
 
 # Create Public IP for Load Balancer
