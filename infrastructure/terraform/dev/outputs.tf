5b0df8c6d37d7c963fa4281aa1f34b5e550f732c
modpanyakorn <panyakorn.mod@gmail.com>
2025-08-26 04:22:03 +0700

    update: add templatefile for render hots.ini (access VMs)

--- infrastructure/terraform/dev/outputs.tf
+++ infrastructure/terraform/dev/outputs.tf
@@ -1,19 +1,30 @@
 output "vm_ips" {
   value = {
-    haproxy_public   = module.haproxy_vm.external_ip
-    haproxy_private  = module.haproxy_vm.internal_ip
-    frontend_public  = module.frontend_vm.external_ip
-    frontend_private = module.frontend_vm.internal_ip
-    backend_public   = module.backend_vm.external_ip
-    backend_private  = module.backend_vm.internal_ip
-    db_private       = module.db_vm.internal_ip
-    devops_public    = module.devops_vm.external_ip
-    devops_private   = module.devops_vm.internal_ip
+    nginx_proxy_public  = module.nginx_vm.external_ip
+    nginx_proxy_private = module.nginx_vm.internal_ip
+    frontend_private    = module.frontend_vm.internal_ip
+    backend_private     = module.backend_vm.internal_ip
+    db_private          = module.db_vm.internal_ip
+    devops_private      = module.devops_vm.internal_ip
+    devops_public       = module.devops_vm.external_ip
+    load_balance        = google_compute_global_address.devops_lb_static_ip.address
   }
 }
 
-# Load Balancer
-output "devops_lb_ip" {
-  description = "The public IP address of the DevOps IAP Load Balancer"
-  value       = google_compute_global_address.devops_lb_static_ip.address
+# mapping key
+locals {
+  value = templatefile("../../ansible/templates/hosts.ini.tftpl", {
+    project          = var.project_id
+    zone             = var.zone
+    nginx_proxy_name = module.nginx_vm.instance_name
+    frontend_name    = module.frontend_vm.instance_name
+    backend_name     = module.backend_vm.instance_name
+    db_name          = module.db_vm.instance_name
+    devops_name      = module.devops_vm.instance_name
+  })
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "../../ansible/inventory/hosts.ini"
+  content  = local.value
 }
