1b5609761fd58092bc3d67a9e1d3ebda14888eb6
modpanyakorn <panyakorn.mod@gmail.com>
2025-08-26 04:21:31 +0700

    update: add variables

--- infrastructure/terraform/dev/variables.tf
+++ infrastructure/terraform/dev/variables.tf
@@ -24,3 +24,39 @@ variable "members" {
   description = "list of team members' emails for IAM roles"
   type        = list(string)
 }
+
+variable "domains" {
+  description = "List of domains for default service (Terraform needed)"
+  type        = list(string)
+}
+
+variable "grafana_sub_domain" {
+  description = "sub domains for Grafana"
+  type        = list(string)
+}
+
+variable "prometheus_sub_domain" {
+  description = "List of domains for Prometheus"
+  type        = list(string)
+}
+
+variable "jenkins_sub_domain" {
+  description = "List of domains for Jenkins"
+  type        = list(string)
+}
+
+variable "sonarqube_sub_domain" {
+  description = "List of domains for Sonarqube"
+  type        = list(string)
+}
+
+variable "mongo_express_sub_domain" {
+  description = "List of domains for mongo_express"
+  type        = list(string)
+}
+
+variable "cadvisor_sub_domain" {
+  description = "List of domains for cadvisor"
+  type        = list(string)
+}
+
