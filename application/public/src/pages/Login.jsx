751a87be491180bed26f08328d287c0893917b30
modpanyakorn <panyakorn.mod@gmail.com>
2025-08-28 05:42:50 +0700

    add: let's gooo~~

--- application/public/src/pages/Login.jsx
+++ application/public/src/pages/Login.jsx
@@ -84,7 +84,8 @@ export default function Login() {
           />
           <button type="submit">Log In</button>
           <span>
-            Don't have an account let's <Link to="/register">Create One.</Link>
+            Don't have an account let's gooo~~{" "}
+            <Link to="/register">Create One.</Link>
           </span>
         </form>
       </FormContainer>
